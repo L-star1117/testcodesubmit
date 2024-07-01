85dfbe6e21250eb4dafda8f84553d7811e5db4a6
itcast <hello@itcast.cn>
2024-07-01 09:54:35 +0800

    第二次修改20240701

--- src/main/java/test01.java
+++ src/main/java/test01.java
@@ -1,6 +1,6 @@
 public class test01 {
     public static void main(String[] args) {
 //        System.out.println("20240701 submittest");
-        System.out.println("20240701 submittest 第二次提交");
+        System.out.println("20240701 submittest 第二次提交111");
     }
 }
